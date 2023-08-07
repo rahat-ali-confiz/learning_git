2b2cdfcc37586ab759802e46c7d4fbd8f0a8f5b3
rahat.ali <rahat.ali@confiz.com>
2023-08-07 15:02:30 +0500

    Added Authentication

---
+++ auth.js
@@ -0,0 +1,9 @@
+function authentication(login, password) {
+	if(login === 'login' && password === 'password') {
+		return 'You are logged in';
+
+	} else {
+	  return 'Login is not correct'
+	}
+		
+  }	
